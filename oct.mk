3538bd47a4e680df547c13a1f04c965d4672e85d
Carl Windhausen <cjwindhausen@gmail.com>
2013-11-18 11:17:38 -0500

    build update

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common OCT stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecri TARGET_DEVICE=jfltecri BUILD_FINGERPRINT="samsung/oct_jfltecri/jfltecri:4.4/KRT16M/user/release-keys" PRIVATE_BUILD_DESC="jfltecri-user 4.4 KRT16M release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecri TARGET_DEVICE=jfltecri BUILD_FINGERPRINT="samsung/oct_jfltecri/jfltecri:4.4/KRT16O/user/release-keys" PRIVATE_BUILD_DESC="jfltecri-user 4.4 KRT16O release-keys"
 
 PRODUCT_DEVICE := jfltecri
 PRODUCT_NAME := oct_jfltecri
