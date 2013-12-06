5df208f4d95b8e8dfd15f0469f168a8d6b27f7ce
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-06 10:59:57 -0500

    Did someone say 4.4.1_r1? KOT49E

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 # Inherit some common OCT stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecri TARGET_DEVICE=jfltecri BUILD_FINGERPRINT="samsung/oct_jfltecri/jfltecri:4.4/KRT16S/user/release-keys" PRIVATE_BUILD_DESC="jfltecri-user 4.4 KRT16S release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecri TARGET_DEVICE=jfltecri BUILD_FINGERPRINT="samsung/oct_jfltecri/jfltecri:4.4.1/KOT49E/user/release-keys" PRIVATE_BUILD_DESC="jfltecri-user 4.4.1 KOT49E release-keys"
 
 PRODUCT_DEVICE := jfltecri
 PRODUCT_NAME := oct_jfltecri
